a364c7169443cdaff1eae45c6700da4ffc6a92f6
Esandi Diddugoda <esandididdugoda4@gmail.com>
2026-02-19 23:43:00 +0530

    Add catalog page frontend. Still have minor changes to be done in the footer part.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Catalog } from './pages/catalog/catalog';
+import { ProductDetail } from './pages/product-detail/product-detail';
+import { Cart } from './pages/cart/cart';
+import { Checkout } from './pages/checkout/checkout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'catalog', pathMatch: 'full' },
+  { path: 'catalog', component: Catalog },
+  { path: 'product-detail', component: ProductDetail },
+  { path: 'cart', component: Cart },
+  { path: 'checkout', component: Checkout }
+];
---
+++ src/app/pages/catalog/catalog.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog',
+  imports: [],
+  templateUrl: './catalog.html',
+  styleUrl: './catalog.css',
+})
+export class Catalog {
+
+}
