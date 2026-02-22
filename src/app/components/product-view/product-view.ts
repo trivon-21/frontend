51283358ac9a57ff714f3cc6b4cd51dcdac6329b
Sathnuwan Dassana <95568551+sDassana@users.noreply.github.com>
2026-02-22 20:47:36 +0530

    Add product view component with HTML, CSS, and test files

---
+++ src/app/components/product-view/product-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-view',
+  imports: [],
+  templateUrl: './product-view.html',
+  styleUrl: './product-view.css',
+})
+export class ProductView {
+
+}
